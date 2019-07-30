4b226863caeeedf0f3cce479fb1b0e0a0733234a
shimingyue1994 <2665867688@qq.com>
2019-07-30 14:00:20 +0800

    提交

--- app/src/main/java/com/yue/camerademo/camera1/CameraTest01Activity.java
+++ app/src/main/java/com/yue/camerademo/camera1/CameraTest01Activity.java
@@ -1,6 +1,7 @@
 package com.yue.camerademo.camera1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Context;
 import android.hardware.Camera;
@@ -12,6 +13,7 @@ import android.view.SurfaceView;
 import android.view.ViewGroup;
 
 import com.yue.camerademo.R;
+import com.yue.camerademo.databinding.ActivityCameraTest01Binding;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,10 +21,16 @@ import java.util.List;
 public class CameraTest01Activity extends AppCompatActivity {
 
     private Camera mCamera;
+    private Preview preview;
+    private ActivityCameraTest01Binding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera_test01);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_camera_test01);
+        preview = new Preview(this);
+        mBinding.flContainer.addView(preview, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        safeCameraOpen(1000);
     }
 
     private boolean safeCameraOpen(int id) {
@@ -30,8 +38,8 @@ public class CameraTest01Activity extends AppCompatActivity {
 
         try {
             releaseCameraAndPreview();
-            camera = Camera.open(id);
-            qOpened = (camera != null);
+            mCamera = Camera.open(id);
+            qOpened = (mCamera != null);
         } catch (Exception e) {
             Log.e(getString(R.string.app_name), "failed to open Camera");
             e.printStackTrace();
@@ -42,9 +50,9 @@ public class CameraTest01Activity extends AppCompatActivity {
 
     private void releaseCameraAndPreview() {
         preview.setCamera(null);
-        if (camera != null) {
-            camera.release();
-            camera = null;
+        if (ca != null) {
+            mCamera.release();
+            mCamera = null;
         }
     }
 
@@ -98,47 +106,50 @@ public class CameraTest01Activity extends AppCompatActivity {
                 mCamera.stopPreview();
             }
         }
-    }
 
-    public void setCamera(Camera camera) {
-        if (mCamera == camera) { return; }
-
-        stopPreviewAndFreeCamera();
-
-        mCamera = camera;
-
-        if (mCamera != null) {
-            List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
-            supportedPreviewSizes = localSizes;
-            requestLayout();
-
-            try {
-                mCamera.setPreviewDisplay(holder);
-            } catch (IOException e) {
-                e.printStackTrace();
+        public void setCamera(Camera camera) {
+            if (mCamera == camera) {
+                return;
             }
 
-            // Important: Call startPreview() to start updating the preview
-            // surface. Preview must be started before you can take a picture.
-            mCamera.startPreview();
+            stopPreviewAndFreeCamera();
+
+            mCamera = camera;
+
+            if (mCamera != null) {
+                List<Camera.Size> localSizes = mCamera.getParameters().getSupportedPreviewSizes();
+                supportedPreviewSizes = localSizes;
+                requestLayout();
+
+                try {
+                    mCamera.setPreviewDisplay(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // Important: Call startPreview() to start updating the preview
+                // surface. Preview must be started before you can take a picture.
+                mCamera.startPreview();
+            }
+        }
+
+        /**
+         * When this function returns, mCamera will be null.
+         */
+        private void stopPreviewAndFreeCamera() {
+
+            if (mCamera != null) {
+                // Call stopPreview() to stop updating the preview surface.
+                mCamera.stopPreview();
+
+                // Important: Call release() to release the camera for use by other
+                // applications. Applications should release the camera immediately
+                // during onPause() and re-open() it during onResume()).
+                mCamera.release();
+
+                mCamera = null;
+            }
         }
     }
 
-    /**
-     * When this function returns, mCamera will be null.
-     */
-    private void stopPreviewAndFreeCamera() {
-
-        if (mCamera != null) {
-            // Call stopPreview() to stop updating the preview surface.
-            mCamera.stopPreview();
-
-            // Important: Call release() to release the camera for use by other
-            // applications. Applications should release the camera immediately
-            // during onPause() and re-open() it during onResume()).
-            mCamera.release();
-
-            mCamera = null;
-        }
-    }
 }
